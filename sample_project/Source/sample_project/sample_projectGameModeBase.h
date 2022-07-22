63a51301f93b17ba7b28c315c7c5194562f15e11
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2022-07-22 16:14:14 -0700

    Update sample_projectGameModeBase.h

--- sample_project/Source/sample_project/sample_projectGameModeBase.h
+++ sample_project/Source/sample_project/sample_projectGameModeBase.h
@@ -17,8 +17,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "editor.h"
-#include "Http.h"
 #include "sample_projectGameModeBase.generated.h"
 
 
@@ -26,10 +24,4 @@ UCLASS(Blueprintable)
 class SAMPLE_PROJECT_API Asample_projectGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-public:
-	virtual void StartPlay() override;
-private:
-
 };
-
-
