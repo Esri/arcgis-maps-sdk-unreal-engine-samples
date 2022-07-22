6e0c2298d0f5e0e819f997bd912be811c1253f30
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2022-07-22 11:25:25 -0700

    Revert "Remove Routing Sample Code"
    This reverts commit eaf63b8356a05aed2b72749f2b36008ff57850e4.

---
+++ sample_project/Source/sample_project/Breadcrumb.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "Breadcrumb.generated.h"
+
+UCLASS()
+class SAMPLE_PROJECT_API ABreadcrumb : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABreadcrumb();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "ArcGISMapsSDK|SampleDefaultPawn")
+	UArcGISLocationComponent* ArcGISLocation;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector3d MeshScale = FVector3d(7.);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+};
---
+++ sample_project/Source/sample_project/RouteMarker.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "RouteMarker.generated.h"
+
+UCLASS()
+class SAMPLE_PROJECT_API ARouteMarker : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARouteMarker();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "ArcGISMapsSDK|SampleDefaultPawn")
+	UArcGISLocationComponent* ArcGISLocation;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector3d MeshScale = FVector3d(15.);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+};
