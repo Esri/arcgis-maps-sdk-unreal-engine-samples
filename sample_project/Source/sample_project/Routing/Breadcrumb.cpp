c398e6562a54483c145af474ccd4c6b3585c965e
Arash Shahbaz Badr <shahbaar@oregonstate.edu>
2022-07-22 11:39:35 -0700

    Co-authored-by: Matthew Nelson <m.nelson@esri.com>
    Formatting changes and copyright info

--- sample_project/Source/sample_project/Routing/Breadcrumb.cpp
+++ sample_project/Source/sample_project/Routing/Breadcrumb.cpp
@@ -1,5 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+/* Copyright 2022 Esri
+ *
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "Breadcrumb.h"
 
@@ -24,14 +36,12 @@ ABreadcrumb::ABreadcrumb()
 
 	ArcGISLocation = CreateDefaultSubobject<UArcGISLocationComponent>(TEXT("ArcGISLocation"));
 	ArcGISLocation->SetupAttachment(Root);
-
 }
 
 // Called when the game starts or when spawned
 void ABreadcrumb::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
