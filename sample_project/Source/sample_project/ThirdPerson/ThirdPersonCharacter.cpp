3acd4cda5eb62ef13093707a60d964688d4696e2
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2023-07-19 10:08:31 -0700

    Mas12517/user interface/3rd person UI (#46)
    * Initial commit
    
    * Sample UI Creation
    
    * User Interface Work
    
    * New User Interface
    
    * Attempt to fix Sample Data Node Error
    
    * Change Default Gamemode
    
    * Fix Animation
    
    * Sample Viewer User interface
    
    * New Third Person Character Controller
    
    * Map Man can fly!
    
    * Fix video issue
    
    * Fix .Uproject
    
    * Address Feedback
    
    * Address Feedback
    
    * Fix Uproject
    
    * Revert "Merge branch 'mas12517/SampleViewerUI' of https://github.com/Esri/arcgis-maps-sdk-unreal-engine-samples into mas12517/User-Interface/3rdPersonUI"
    
    This reverts commit 87efefd540514241b95cdf6c5776ad37d459fb1f, reversing
    changes made to 241987a32f411d8f86d2c67e17dc0ce2e7f7a8dc.
    
    * Fix Animation
    
    * Loop Video
    
    * Fix Auto Compile error
    
    * Fix animation speed and quit to desktop button position
    
    * Edit Close Menu Animation
    
    * Address Andy Feedback
    
    * Scale Cancel Icon Down
    
    * Format Code and make functions private
    
    * Update to Third Person Character UI and Add Sprinting
    
    * Replace Info Image
    
    * Fix UI Layout
    
    * Disable Camera Lag
    
    * Fix Margins
    
    ---------
    
    Co-authored-by: mas12517 <mgaw@esri.com>

---
+++ sample_project/Source/sample_project/ThirdPerson/ThirdPersonCharacter.cpp
@@ -0,0 +1,188 @@
+/* Copyright 2022 Esri
+*
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "ThirdPersonCharacter.h"
+
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "Components/CapsuleComponent.h"
+
+
+AThirdPersonCharacter::AThirdPersonCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 0.0f, 360.0f);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UArcGISCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+void AThirdPersonCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem
+			<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+		}
+	}
+}
+
+void AThirdPersonCharacter::JumpActionEvent(const FInputActionValue& value)
+{
+	Jump();
+}
+
+void AThirdPersonCharacter::StopJumpActionEvent(const FInputActionValue& value)
+{
+	StopJumping();
+}
+
+void AThirdPersonCharacter::SetCameraBoomSettings()
+{
+	if (GetCharacterMovement()->IsFlying())
+	{
+		CameraBoom->SocketOffset.Z = 75.0f;
+	}
+	else
+	{
+		CameraBoom->SocketOffset.Z = 0.0f;
+	}
+}
+
+
+void AThirdPersonCharacter::StartFlying(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<bool>();
+	if(inputValue)
+	{
+		if (!flying)
+		{
+			GetCharacterMovement()->SetMovementMode(MOVE_Flying, 0);
+			flying = true;
+		}
+		else if(flying)
+		{
+			GetCharacterMovement()->SetMovementMode(MOVE_Walking, 0);
+			flying = false;
+		}
+		SetCameraBoomSettings();
+	}
+}
+
+void AThirdPersonCharacter::Look(const FInputActionValue& value)
+{
+	FVector2D lookAxisValue = value.Get<FVector2D>();
+	
+	if (GetController())
+	{
+		AddControllerYawInput(lookAxisValue.X);
+		AddControllerPitchInput(-lookAxisValue.Y);
+	}
+}
+
+void AThirdPersonCharacter::MoveForward(const FInputActionValue& value)
+{
+	const auto inputValue = value.Get<float>();
+	FRotator Rotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+	FVector Direction = GetActorForwardVector();
+	
+	if (GetCharacterMovement()->IsFlying())
+	{
+		AddMovementInput(Direction, inputValue);
+	}
+	else
+	{
+		AddMovementInput(Direction, inputValue);
+	}
+}
+
+void AThirdPersonCharacter::MoveRight(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<float>();
+	FRotator Rotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+	FVector Direction = GetActorRightVector();
+	
+	if (GetCharacterMovement()->IsFlying())
+	{
+		AddMovementInput(Direction, inputValue);
+	}
+	else
+	{
+		AddMovementInput(Direction, inputValue);
+	}
+}
+
+void AThirdPersonCharacter::MoveUp(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<float>();
+	
+	if (GetCharacterMovement()->IsFlying())
+	{
+		AddMovementInput(GetActorUpVector(), inputValue * 10000.0f);
+	}
+}
+
+void AThirdPersonCharacter::Sprint(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<bool>();
+	
+	if(inputValue)
+	{
+		if(!sprinting)
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 2000.0f;
+			sprinting = true;
+		}
+		else if(sprinting)
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+			sprinting = false;
+		}	
+	}
+}
+
+
+void AThirdPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AThirdPersonCharacter::Look);
+		EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &AThirdPersonCharacter::MoveForward);
+		EnhancedInputComponent->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &AThirdPersonCharacter::MoveRight);
+		EnhancedInputComponent->BindAction(MoveUpAction, ETriggerEvent::Triggered, this, &AThirdPersonCharacter::MoveUp);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AThirdPersonCharacter::JumpActionEvent);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &AThirdPersonCharacter::StopJumpActionEvent);
+		EnhancedInputComponent->BindAction(StartFlyingAction, ETriggerEvent::Started, this, &AThirdPersonCharacter::StartFlying);
+		EnhancedInputComponent->BindAction(SprintingAction, ETriggerEvent::Started, this, &AThirdPersonCharacter::Sprint);
+	}
+}
---
+++ sample_project/Source/sample_project/ThirdPerson/ThirdPersonCharacter.h
@@ -0,0 +1,75 @@
+/* Copyright 2022 Esri
+*
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
+#include "ArcGISMapsSDK/Components/ArcGISCameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "ThirdPersonCharacter.generated.h"
+
+UCLASS()
+class SAMPLE_PROJECT_API AThirdPersonCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AThirdPersonCharacter();
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		UArcGISCameraComponent* FollowCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputMappingContext* MappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputAction* JumpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputAction* MoveForwardAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputAction* MoveRightAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		 UInputAction* LookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputAction* MoveUpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputAction* StartFlyingAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+		UInputAction* SprintingAction;
+private:
+	bool sprinting;
+	bool flying;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:	
+	void SetCameraBoomSettings();
+	void StartFlying(const FInputActionValue& value);
+	void JumpActionEvent(const FInputActionValue& value);
+	void StopJumpActionEvent(const FInputActionValue& value);
+	void Look(const FInputActionValue& value);
+	void MoveUp(const FInputActionValue& value);
+	void MoveForward(const FInputActionValue& value);
+	void MoveRight(const FInputActionValue& value);
+	void Sprint(const FInputActionValue& value);
+
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
