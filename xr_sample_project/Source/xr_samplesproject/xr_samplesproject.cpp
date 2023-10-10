9f6733283c87902a2257967242728cefa14768fc
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2023-10-10 11:30:13 -0700

    Split samples into general samples project and xr samples project (#54)
    * XR Samples Project and updated Gitignore
    
    * ignore .vsconfig
    
    * Revert sample_project.uproject
    
    * Update gitignore
    
    * revert defaultgame.ini

---
+++ xr_sample_project/Source/xr_samplesproject/xr_samplesproject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "xr_samplesproject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, xr_samplesproject, "xr_samplesproject" );
