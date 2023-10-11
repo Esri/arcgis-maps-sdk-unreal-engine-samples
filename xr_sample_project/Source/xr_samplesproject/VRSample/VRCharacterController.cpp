916da25cf4cd8a27fe10bc73932748f5185d76ce
mas12517 <89485705+mmgaw113@users.noreply.github.com>
2023-10-11 13:49:45 -0700

    VR Sample

--- xr_sample_project/Source/xr_samplesproject/VRSample/VRCharacterController.cpp
+++ xr_sample_project/Source/xr_samplesproject/VRSample/VRCharacterController.cpp
@@ -2,18 +2,20 @@
 
 
 #include "VRCharacterController.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AVRCharacterController::AVRCharacterController()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 	VROrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VROrigin"));
 	VROrigin->SetupAttachment(RootComponent);
 	VRCamera = CreateDefaultSubobject<UArcGISCameraComponent>(TEXT("FollowCamera"));
 	VRCamera->SetupAttachment(VROrigin);
 	VRCamera->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
+	VRCamera->bUsePawnControlRotation = true;
 	
 	LocationComponent = CreateDefaultSubobject<UArcGISLocationComponent>(TEXT("Location Component"));
 	LocationComponent->SetupAttachment(RootComponent);
@@ -21,14 +23,20 @@ AVRCharacterController::AVRCharacterController()
 	LeftMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftMotionController"));
 	LeftMotionController->SetupAttachment(VROrigin);
 	LeftMotionController->SetTrackingSource(EControllerHand::Left);
-	LeftHandMesh->CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Left Hand Mesh"));
+	LeftHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Left Hand Mesh"));
 	LeftHandMesh->SetupAttachment(LeftMotionController);
+	LeftHandMesh->RegisterComponent();
+	LeftHandMesh->SetSkeletalMesh(LeftMesh);
+	LeftHandMesh->SetRelativeRotation(FRotator(-90.0f, -90.0f, 0.0f));
 	
 	RightMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightMotionController"));
 	RightMotionController->SetupAttachment(VROrigin);
 	RightMotionController->SetTrackingSource(EControllerHand::Right);
-	RightHandMesh->CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Right Hand Mesh"));
+	RightHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Right Hand Mesh"));
 	RightHandMesh->SetupAttachment(RightMotionController);
+	RightHandMesh->RegisterComponent();
+	RightHandMesh->SetSkeletalMesh(RightMesh);
+	RightHandMesh->SetRelativeRotation(FRotator(90.0f, -90.0f, 0.0f));  
 
 }
 
@@ -76,11 +84,50 @@ void AVRCharacterController::MoveUp(const FInputActionValue& value)
 	}
 }
 
+void AVRCharacterController::SmoothTurn(const FInputActionValue& value)
+{
+	auto InputValue = value.Get<float>();
+
+	if(abs(InputValue) > TurnDeadZone)
+	{
+		const auto RotationAngle = SnapRotationDegrees;
+		SetActorRotation(FRotator(0.0f, 0.0f, GetActorRotation().Yaw * RotationSpeed + RotationAngle));
+	}
+}  
+
+
+void AVRCharacterController::SnapTurn(const FInputActionValue& value)
+{
+	auto InputValue = value.Get<float>();
+	auto RotationAngle = 0.0f;
+	
+	if(abs(InputValue) > TurnDeadZone)
+	{
+		RotationAngle = SnapRotationDegrees;
+		SetActorRotation(FRotator(0.0f, 0.0f, GetActorRotation().Yaw + RotationAngle));
+	}
+	else
+	{
+		RotationAngle = SnapRotationDegrees * -1.0f;
+		SetActorRotation(FRotator(0.0f, 0.0f, GetActorRotation().Yaw + RotationAngle));
+	}
+}
+
+
+void AVRCharacterController::UpdateRoomScaleMovement()
+{
+	FVector Offset = VRCamera->GetComponentLocation() - VROrigin->GetComponentLocation();
+	AddActorWorldOffset(FVector(Offset.X, Offset.Y, 0.0f));
+	VROrigin->AddWorldOffset(UKismetMathLibrary::NegateVector(FVector(Offset.X, Offset.Y, 0.0f)));
+}
+
+
 // Called when the game starts or when spawned
 void AVRCharacterController::BeginPlay()
 {
 	Super::BeginPlay();
-
+	GetCharacterMovement()->SetMovementMode(MOVE_Flying, 0);
+	
 	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
 	{
 		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem
@@ -89,6 +136,9 @@ void AVRCharacterController::BeginPlay()
 			Subsystem->AddMappingContext(MappingContext, 0);
 		}
 	}
+
+	FTimerHandle UpdateHeight;
+	GetWorldTimerManager().SetTimer(UpdateHeight, this, &AVRCharacterController::UpdateRoomScaleMovement, 0.3f, true);
 }
 
 // Called every frame
@@ -103,7 +153,17 @@ void AVRCharacterController::SetupPlayerInputComponent(UInputComponent* PlayerIn
 {
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		//EnhancedInputComponent->BindAction();
+		EnhancedInputComponent->BindAction(Move_X, ETriggerEvent::Triggered, this, &AVRCharacterController::MoveRight);
+		EnhancedInputComponent->BindAction(Move_Y, ETriggerEvent::Triggered, this, &AVRCharacterController::MoveForward);
+		EnhancedInputComponent->BindAction(Move_Z, ETriggerEvent::Triggered, this, &AVRCharacterController::MoveUp);
+		if(bUseSmoothTurn)
+		{
+			EnhancedInputComponent->BindAction(Turn, ETriggerEvent::Triggered, this, &AVRCharacterController::SmoothTurn);
+		}
+		else
+		{
+			EnhancedInputComponent->BindAction(Turn, ETriggerEvent::Triggered, this, &AVRCharacterController::SnapTurn);
+		}
 	}
 
 }
--- xr_sample_project/Source/xr_samplesproject/VRSample/VRCharacterController.h
+++ xr_sample_project/Source/xr_samplesproject/VRSample/VRCharacterController.h
@@ -4,13 +4,15 @@
 
 #include "ArcGISMapsSDK/Components/ArcGISCameraComponent.h"
 #include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "CoreMinimal.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "InputCoreTypes.h"
+#include "InputMappingContext.h"
 #include "MotionControllerComponent.h"
-#include "GameFramework/Character.h"
 #include "VRCharacterController.generated.h"
 
 UCLASS()
@@ -30,26 +32,49 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	USkeletalMeshComponent* LeftHandMesh;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	USkeletalMesh* LeftMesh = LoadObject<USkeletalMesh>(nullptr,TEXT("/Game/Samples/VRSample/Hands/Meshes/SKM_MannyXR_left.SKM_MannyXR_left"));
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	USkeletalMeshComponent* RightHandMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	USkeletalMesh* RightMesh = LoadObject<USkeletalMesh>(nullptr,TEXT("/Game/Samples/VRSample/Hands/Meshes/SKM_MannyXR_right.SKM_MannyXR_right"));
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	UMotionControllerComponent* RightMotionController;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	USceneComponent* VROrigin;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
-	UInputMappingContext* MappingContext;
-
+	UInputMappingContext* MappingContext = LoadObject<UInputMappingContext>(nullptr, TEXT("/Game/Samples/VRSample/Input/IMC_Default.IMC_Default"));
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+	UInputAction* Move_X = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_MoveX.IA_MoveX"));
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+	UInputAction* Move_Y = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_MoveY.IA_MoveY"));
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+	UInputAction* Move_Z = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_MoveUp.IA_MoveUp"));
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enhanced Input")
+	UInputAction* Turn = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_Turn.IA_Turn"));
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Variables")
+	bool bUseSmoothTurn = true;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Variables")
+	float RotationSpeed = 1.0f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Variables")
+	float SnapRotationDegrees = 30.0f;
+	float TurnDeadZone = 0.0f;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:
-	void MoveForward(const FInputActionValue& value);
-	void MoveRight(const FInputActionValue& value);
-	void MoveUp(const FInputActionValue& value);
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void MoveForward(const FInputActionValue& value);
+	void MoveRight(const FInputActionValue& value);
+	void MoveUp(const FInputActionValue& value);
+	void SmoothTurn(const FInputActionValue& value);
+	void SnapTurn(const FInputActionValue& value);
+	void UpdateRoomScaleMovement();
 };
