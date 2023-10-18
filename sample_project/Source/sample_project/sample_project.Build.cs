a6638ed47a2f79cf601ef4ef21f6985357161dc5
mas12517 <89485705+mmgaw113@users.noreply.github.com>
2023-10-18 15:38:28 -0700

    Revert sample_project.Build.cs

--- sample_project/Source/sample_project/sample_project.Build.cs
+++ sample_project/Source/sample_project/sample_project.Build.cs
@@ -22,7 +22,7 @@ public class sample_project : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG",
-			"HTTP", "Json","JsonUtilities", "EnhancedInput", "ArcGISMapsSDK", "ArcGISSamples", "WebSockets"
+			"HTTP", "Json","JsonUtilities", "EnhancedInput", "ArcGISMapsSDK", "ArcGISSamples"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
