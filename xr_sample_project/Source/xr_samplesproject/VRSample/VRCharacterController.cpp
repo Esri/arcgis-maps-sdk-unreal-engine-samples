290802b95e9b0603ce694f6a80ac62f2ba267859
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2023-10-24 15:32:35 -0700

    Mas12517/vr sample (#56)
    * Feature Layer UI Redesign
    
    * Update sizing, remove old instructions.
    
    * Updates to UI
    
    * Feature Layer Updates
    
    * Feature Layer UI
    
    * Change File names
    
    * Fix Issue
    
    * Update Widget
    
    * Level Check in Game Mode
    
    * Stream Layer
    
    * Fix Outfields Bug
    
    * Update instruction and warning texts
    
    * Add delay for getting outfields
    
    * dynamic scaling of feature items
    
    * Update Readme
    
    * Update Links
    
    * Fix broken link
    
    * Server Setup
    
    * Feature Layer UI Redesign
    
    * Reset map
    
    * Fix UI bug
    
    * Deadreckoning setup
    
    * Deadreckoning script
    
    * Spawning Actors
    
    * Stream Layer spawning Loop
    
    * Stream Layer Sample Updates
    
    * Stream Layer Query
    
    * VR sample Setup
    
    * VR Sample
    
    * Remove IA_Move
    
    * Smooth turning and bug fixes
    
    * Updates to snap turning function
    
    * Updates to VR Controller
    
    * Vignette for movement
    
    * Hand Component
    
    * Vignette
    
    * Revert "Hand Component"
    
    This reverts commit 722a235f24b5e0ecf3bb251f9e2a06ca3b28d027.
    
    * VR Sample Hands
    
    * Remove Stream Layer Sample
    
    * Revert sample_project.Build.cs
    
    * Fix Hand animations and update Vignette.
    
    * Address Feedback
    
    ---------
    
    Co-authored-by: Daniel Boghossian <dboghossian@esri.com>

---
+++ xr_sample_project/Source/xr_samplesproject/VRSample/VRCharacterController.cpp
@@ -0,0 +1,298 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRCharacterController.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+AVRCharacterController::AVRCharacterController()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	vrOrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VROrigin"));
+	vrOrigin->SetupAttachment(RootComponent);
+	vrCamera = CreateDefaultSubobject<UArcGISCameraComponent>(TEXT("FollowCamera"));
+	vrCamera->SetupAttachment(vrOrigin);
+	vrCamera->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
+	
+	locationComponent = CreateDefaultSubobject<UArcGISLocationComponent>(TEXT("Location Component"));
+	locationComponent->SetupAttachment(RootComponent);
+
+	leftMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftMotionController"));
+	leftMotionController->SetupAttachment(vrOrigin);
+	leftMotionController->SetTrackingSource(EControllerHand::Left);
+	leftHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Left Hand Mesh"));
+	leftHandMesh->SetupAttachment(leftMotionController);
+	leftHandMesh->RegisterComponent();
+	leftHandMesh->SetSkeletalMesh(handMesh);
+	leftHandMesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+	leftHandMesh->SetAnimClass(handAnimBP->GeneratedClass);
+	
+	rightMotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightMotionController"));
+	rightMotionController->SetupAttachment(vrOrigin);
+	rightMotionController->SetTrackingSource(EControllerHand::Right);
+	rightHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Right Hand Mesh"));
+	rightHandMesh->SetupAttachment(rightMotionController);
+	rightHandMesh->RegisterComponent();
+	rightHandMesh->SetSkeletalMesh(handMesh);
+	rightHandMesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+	rightHandMesh->SetAnimClass(handAnimBP->GeneratedClass);
+}
+
+void AVRCharacterController::MoveForward(const FInputActionValue& value)
+{
+	const auto inputValue = value.Get<float>();
+
+	if (abs(inputValue) > MovementDeadzone)
+	{
+		if (bMoveInLookDirection)
+		{
+			FVector Direction = vrCamera->GetForwardVector();
+			AddMovementInput(Direction, inputValue * MoveSpeed);
+		}
+		else 
+		{
+			FVector direction = rightMotionController->GetForwardVector();
+			AddMovementInput(direction, inputValue * MoveSpeed);
+		}
+	}
+}
+
+void AVRCharacterController::MoveRight(const FInputActionValue& value)
+{
+	const auto inputValue = value.Get<float>();
+
+	if (abs(inputValue) > MovementDeadzone)
+	{
+		if (bMoveInLookDirection)
+		{
+			FVector Direction = vrCamera->GetRightVector();
+			AddMovementInput(Direction, inputValue * MoveSpeed);
+		}
+		else
+		{
+			FVector direction = rightMotionController->GetRightVector();
+			AddMovementInput(direction, inputValue * MoveSpeed);
+		}
+	}
+}
+
+void AVRCharacterController::MoveUp(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<float>();
+	
+	if (abs(inputValue) > MovementDeadzone)
+	{
+		AddMovementInput(GetActorUpVector(), inputValue * UpSpeed);
+	}
+}
+
+void AVRCharacterController::SmoothTurn(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<float>() * RotationSpeed;
+
+	if (abs(inputValue) > RotationDeadzone)
+	{
+		SetActorRotation(FRotator(0.0f, GetActorRotation().Yaw + inputValue, 0.0f));
+	}
+}
+
+void AVRCharacterController::SnapTurn(const FInputActionValue& value)
+{
+	auto inputValue = value.Get<float>();
+
+	if(bDoOnce && abs(inputValue) > RotationDeadzone)
+	{
+		auto RotationAngle = 0.0f;
+		if(inputValue > 0.0f)
+		{
+			RotationAngle = SnapRotationDegrees;
+			SetActorRotation(FRotator(0.0f, GetActorRotation().Yaw + RotationAngle, 0.0f));
+		}
+		else
+		{
+			RotationAngle = SnapRotationDegrees * -1.0f;
+			SetActorRotation(FRotator(0.0f, GetActorRotation().Yaw + RotationAngle, 0.0f));
+		}
+		bDoOnce = false;
+	}
+}
+
+void AVRCharacterController::ResetDoOnce()
+{
+	if(!bDoOnce)
+	{
+		bDoOnce = true;
+	}
+}
+
+void AVRCharacterController::InitializeCapsuleHeight() 
+{
+	capsuleHalfHeight = GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+}
+
+void AVRCharacterController::SetCapsuleHeight() 
+{
+	FVector DevicePosition;
+	FQuat DeviceRotation;
+	GEngine->XRSystem->GetCurrentPose(IXRTrackingSystem::HMDDeviceId, DeviceRotation, DevicePosition);
+	GetCapsuleComponent()->SetCapsuleSize(GetCapsuleComponent()->GetScaledCapsuleRadius(), capsuleHalfHeight, true);
+	auto halfHeight = DevicePosition.Z / 2.0f + 10.0f;
+	vrOrigin->AddRelativeLocation(FVector(0.0f, 0.0f, capsuleHalfHeight - halfHeight));
+	capsuleHalfHeight = halfHeight;
+}
+
+void AVRCharacterController::SetLeftGripAxis(const FInputActionValue& value)
+{
+	if (leftAnimInstance)
+	{
+		leftAnimInstance->GripAxis = value.Get<float>();
+	}
+}
+
+void AVRCharacterController::SetRightGripAxis(const FInputActionValue& value)
+{
+	if (rightAnimInstance)
+	{
+		rightAnimInstance->GripAxis = value.Get<float>();
+	}
+}
+
+void AVRCharacterController::SetLeftTriggerAxis(const FInputActionValue& value)
+{
+	if (leftAnimInstance)
+	{
+		leftAnimInstance->TriggerAxis = value.Get<float>();
+	}
+}
+
+void AVRCharacterController::SetRightTriggerAxis(const FInputActionValue& value)
+{	
+	if (rightAnimInstance)
+	{
+		rightAnimInstance->TriggerAxis = value.Get<float>();
+	}
+}
+
+void AVRCharacterController::ResetLeftGripAxis()
+{
+	if (leftAnimInstance)
+	{
+		leftAnimInstance->GripAxis = 0.0f;
+	}
+}
+
+void AVRCharacterController::ResetRightGripAxis()
+{
+	if (rightAnimInstance)
+	{
+		rightAnimInstance->GripAxis = 0.0f;
+	}
+}
+
+void AVRCharacterController::ResetLeftTriggerAxis()
+{
+	if (leftAnimInstance)
+	{
+		leftAnimInstance->TriggerAxis = 0.0f;
+	}
+}
+
+void AVRCharacterController::ResetRightTriggerAxis()
+{
+	if (rightAnimInstance)
+	{
+		rightAnimInstance->TriggerAxis = 0.0f;
+	}
+}
+
+void AVRCharacterController::UpdateRoomScaleMovement()
+{
+	FVector Offset = vrCamera->GetComponentLocation() - GetActorLocation();
+	AddActorWorldOffset(FVector(Offset.X, Offset.Y, 0.0f));
+	vrOrigin->AddWorldOffset(UKismetMathLibrary::NegateVector(FVector(Offset.X, Offset.Y, 0.0f)));
+}
+
+// Called when the game starts or when spawned
+void AVRCharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GEngine->XRSystem->SetTrackingOrigin(EHMDTrackingOrigin::Floor);
+	GetCharacterMovement()->SetMovementMode(MOVE_Flying, 0);
+	InitializeCapsuleHeight();
+	
+	leftAnimInstanceBase = leftHandMesh->GetAnimInstance();
+	leftAnimInstance = Cast<UVRHandAnimInstance>(leftAnimInstanceBase);
+	rightAnimInstanceBase = rightHandMesh->GetAnimInstance();
+	rightAnimInstance = Cast<UVRHandAnimInstance>(rightAnimInstanceBase);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem
+			<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(mappingContext, 0);
+		}
+	}
+
+	auto DeviceType = GEngine->XRSystem->GetHMDDevice()->GetHMDName().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Device Type: %s"), *DeviceType);
+	FTimerHandle UpdateHeight;
+	GetWorldTimerManager().SetTimer(UpdateHeight, this, &AVRCharacterController::SetCapsuleHeight, 0.35f, true);
+	FTimerHandle UpdateCapsuleLocation;
+	GetWorldTimerManager().SetTimer(UpdateCapsuleLocation, this, &AVRCharacterController::UpdateRoomScaleMovement, 0.3f, true);
+}
+
+// Called every frame
+void AVRCharacterController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GetCharacterMovement()->Velocity.Size() > 100)
+	{
+		vrCamera->PostProcessSettings.WeightedBlendables.Array[0].Weight = 1.0;
+	}
+	else 
+	{
+		vrCamera->PostProcessSettings.WeightedBlendables.Array[0].Weight = 0.0;
+	}
+
+}
+
+// Called to bind functionality to input
+void AVRCharacterController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(move_X, ETriggerEvent::Triggered, this, &AVRCharacterController::MoveRight);
+		EnhancedInputComponent->BindAction(move_Y, ETriggerEvent::Triggered, this, &AVRCharacterController::MoveForward);
+		EnhancedInputComponent->BindAction(move_Z, ETriggerEvent::Triggered, this, &AVRCharacterController::MoveUp);
+		//Update Hand Animations
+		EnhancedInputComponent->BindAction(grip_L, ETriggerEvent::Triggered, this, &AVRCharacterController::SetLeftGripAxis);
+		EnhancedInputComponent->BindAction(grip_R, ETriggerEvent::Triggered, this, &AVRCharacterController::SetRightGripAxis);
+		EnhancedInputComponent->BindAction(trigger_L, ETriggerEvent::Triggered, this, &AVRCharacterController::SetLeftTriggerAxis);
+		EnhancedInputComponent->BindAction(trigger_R, ETriggerEvent::Triggered, this, &AVRCharacterController::SetRightTriggerAxis);
+		//Reset Hand Animations on Cancelled Action
+		EnhancedInputComponent->BindAction(grip_L, ETriggerEvent::Canceled, this, &AVRCharacterController::ResetLeftGripAxis);
+		EnhancedInputComponent->BindAction(grip_R, ETriggerEvent::Canceled, this, &AVRCharacterController::ResetRightGripAxis);
+		EnhancedInputComponent->BindAction(trigger_L, ETriggerEvent::Canceled, this, &AVRCharacterController::ResetLeftTriggerAxis);
+		EnhancedInputComponent->BindAction(trigger_R, ETriggerEvent::Canceled, this, &AVRCharacterController::ResetRightTriggerAxis);
+		//Reset Hand Animations on Completed Action
+		EnhancedInputComponent->BindAction(grip_L, ETriggerEvent::Completed, this, &AVRCharacterController::ResetLeftGripAxis);
+		EnhancedInputComponent->BindAction(grip_R, ETriggerEvent::Completed, this, &AVRCharacterController::ResetRightGripAxis);
+		EnhancedInputComponent->BindAction(trigger_L, ETriggerEvent::Completed, this, &AVRCharacterController::ResetLeftTriggerAxis);
+		EnhancedInputComponent->BindAction(trigger_R, ETriggerEvent::Completed, this, &AVRCharacterController::ResetRightTriggerAxis);
+
+		if(bUseSmoothTurn)
+		{
+			EnhancedInputComponent->BindAction(turn, ETriggerEvent::Triggered, this, &AVRCharacterController::SmoothTurn);
+		}
+		else
+		{
+			EnhancedInputComponent->BindAction(turn, ETriggerEvent::Triggered, this, &AVRCharacterController::SnapTurn);
+			EnhancedInputComponent->BindAction(turn, ETriggerEvent::Completed, this, &AVRCharacterController::ResetDoOnce);
+		}
+	}
+}
---
+++ xr_sample_project/Source/xr_samplesproject/VRSample/VRCharacterController.h
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArcGISMapsSDK/Components/ArcGISCameraComponent.h"
+#include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "IHeadMountedDisplay.h"
+#include "InputCoreTypes.h"
+#include "IXRTrackingSystem.h"
+#include "InputMappingContext.h"
+#include "MotionControllerComponent.h"
+#include "VRHandAnimInstance.h"
+#include "VRCharacterController.generated.h"
+
+UCLASS()
+class XR_SAMPLESPROJECT_API AVRCharacterController : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacterController();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	bool bUseSmoothTurn = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	bool bMoveInLookDirection = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	float MovementDeadzone = 0.2f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	float MoveSpeed = 10000.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	float RotationDeadzone = 0.2f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	float RotationSpeed = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	float SnapRotationDegrees = 30.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+	float UpSpeed = 100000.0f;
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void InitializeCapsuleHeight();
+	void MoveForward(const FInputActionValue& value);
+	void MoveRight(const FInputActionValue& value);
+	void MoveUp(const FInputActionValue& value);
+	void ResetDoOnce();
+	void SetCapsuleHeight();
+	void SetLeftGripAxis(const FInputActionValue& value);
+	void SetLeftTriggerAxis(const FInputActionValue& value);
+	void SetRightGripAxis(const FInputActionValue& value);
+	void SetRightTriggerAxis(const FInputActionValue& value);
+	void ResetLeftGripAxis();
+	void ResetLeftTriggerAxis();
+	void ResetRightGripAxis();
+	void ResetRightTriggerAxis();
+	void SmoothTurn(const FInputActionValue& value);
+	void SnapTurn(const FInputActionValue& value);
+	void UpdateRoomScaleMovement();
+
+	bool bDoOnce = true;
+	float capsuleHalfHeight;
+	UAnimBlueprint* handAnimBP = LoadObject<UAnimBlueprint>(nullptr, TEXT("/Game/Samples/VRSample/Hands/Animations/ABP_Hand.ABP_Hand"));
+	USkeletalMesh* handMesh = LoadObject<USkeletalMesh>(nullptr, TEXT("/Game/Samples/VRSample/Hands/Meshes/SKM_MannyXR_left.SKM_MannyXR_left"));
+	UInputAction* grip_L = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_LeftGrip.IA_LeftGrip"));
+	UInputAction* grip_R = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_RightGrip.IA_RightGrip"));
+	UVRHandAnimInstance* leftAnimInstance;
+	UAnimInstance* leftAnimInstanceBase;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	UMotionControllerComponent* leftMotionController;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* leftHandMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	UArcGISLocationComponent* locationComponent;
+	UInputMappingContext* mappingContext = LoadObject<UInputMappingContext>(nullptr, TEXT("/Game/Samples/VRSample/Input/IMC_Default.IMC_Default"));
+	UInputAction* move_X = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_MoveX.IA_MoveX"));
+	UInputAction* move_Y = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_MoveY.IA_MoveY"));
+	UInputAction* move_Z = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_MoveUp.IA_MoveUp"));
+	UVRHandAnimInstance* rightAnimInstance;
+	UAnimInstance* rightAnimInstanceBase;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* rightHandMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	UMotionControllerComponent* rightMotionController;
+	UInputAction* trigger_L = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_LeftTrigger.IA_LeftTrigger"));
+	UInputAction* trigger_R = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_RightTrigger.IA_RightTrigger"));
+	UInputAction* turn = LoadObject<UInputAction>(nullptr, TEXT("/Game/Samples/VRSample/Input/IA_Turn.IA_Turn"));
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	UArcGISCameraComponent* vrCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* vrOrigin;
+};
