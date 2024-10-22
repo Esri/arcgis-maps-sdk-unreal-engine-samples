9d911caf4922e91e1aeff9a0a11a86bf5b659288
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2024-10-22 09:51:30 -0700

    New Sample: C++ Hit Test (#107)
    * Hit Test Redesign
    
    * Add to sample Viewer, fix crash
    
    * Address Early Feedback
    
    * Format
    
    * Fix Up redirectors
    
    * Bug: Build issue with 5.4 (#108)
    
    * Fix build issues and Weather Query bug
    
    * Remove unused local variables
    
    * Revert Weather Query files
    
    * Fix Build issue for all supported UE Versions
    
    * Address Feedback
    
    * Address Feedback
    
    * Address Feedback
    
    * Format Code

---
+++ sample_project/Source/sample_project/HitTest/ArcGISRaycast.h
@@ -0,0 +1,64 @@
+/* Copyright 2024 Esri
+*
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Actor.h"
+#include "Http.h"
+#include "ArcGISRaycast.generated.h"
+
+class UArcGISMapComponent;
+
+UCLASS()
+class SAMPLE_PROJECT_API AArcGISRaycast : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AArcGISRaycast();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent);
+
+private:
+	void CreateLink(FString objectID);
+	void CreateProperties();
+	void GetHit();
+	FString GetObjectIDs(FString response, FString outfield);
+	void OnResponseRecieved(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSucessfully);
+
+	UFunction* createProperties;
+	int featureID;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess))
+	AStaticMeshActor* HitLocation;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess))
+	UInputMappingContext* MappingContext;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess))
+	UInputAction* mousePress;
+	TArray<FString> outfields = {"AREA_SQ_FT", "DISTRICT", "Height", "SUBDISTRIC", "ZONE_"};
+	FString position;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (AllowPrivateAccess))
+	TArray<FString> resultText;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess))
+	UUserWidget* UIWidget;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	TSubclassOf<class UUserWidget> UIWidgetClass;
+	FString webLink;
+};
