c3aeee50636656729b7f6a141f52753ddfc21b74
Stephen Nauman <snauman@esri.com>
2025-01-21 13:47:09 -0800

    Working on scripts

--- sample_project/Source/sample_project/Viewshed/ViewshedMap.cpp
+++ sample_project/Source/sample_project/Viewshed/ViewshedMap.cpp
@@ -1,8 +1,22 @@
-#include "Viewshed.h"
+/* Copyright 2025 Esri
+*
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ViewshedMap.h"
 #include "Engine/World.h"
 #include "TimerManager.h"
 #include "ArcGISMapsSDK/Components/ArcGISMapComponent.h"
-#include "ArcGISMapsSDK/Utils/ArcGISProjectSettingsAsset.h"
+#include "ArcGISMapsSDK/Utils/ArcGISMapsSDKProjectSettings.h"
 
 AViewshed::AViewshed()
 {
@@ -30,9 +44,12 @@ void AViewshed::InitializeMap()
 
 	// Get the API key
 	FString ApiKey = "";
-	if (ApiKey.IsEmpty())
+	if (APIKey.IsEmpty())
 	{
-		ApiKey = UArcGISProjectSettingsAsset::GetArcGISProjectSettings()->APIKey;
+		if (const UArcGISMapsSDKProjectSettings* Settings = GetDefault<UArcGISMapsSDKProjectSettings>())
+		{
+			APIKey = Settings->APIKey;
+		}
 	}
 
 	if (ApiKey.IsEmpty())
