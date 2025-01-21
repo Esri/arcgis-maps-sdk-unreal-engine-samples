3a680b850de022cdba2c6d5d6d5511ceb1443f7a
Stephen Nauman <snauman@esri.com>
2025-01-21 13:47:09 -0800

    Working on scripts

--- sample_project/Source/sample_project/Viewshed/ViewshedCamera.cpp
+++ sample_project/Source/sample_project/Viewshed/ViewshedCamera.cpp
@@ -1,3 +1,17 @@
+/* Copyright 2025 Esri
+*
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "ViewshedCamera.h"
 #include "Components/SceneCaptureComponent2D.h"
 #include "Engine/TextureRenderTarget2D.h"
@@ -24,14 +38,14 @@ void AViewshedCamera::BeginPlay()
 	}
 
 	ViewshedCamera->TextureTarget = DepthTexture;
-	UKismetRenderingLibrary::SetGlobalTextureParameter(this, TEXT("_ArcGISViewshedDepthTex"), DepthTexture);
+	//UKismetRenderingLibrary::SetGlobalTextureParameter(this, TEXT("_ArcGISViewshedDepthTex"), DepthTexture);
 }
 
 void AViewshedCamera::CreateDepthTexture()
 {
 	DepthTexture = NewObject<UTextureRenderTarget2D>(this);
 	DepthTexture->InitAutoFormat(DepthWidth, DepthHeight);
-	DepthTexture->RenderTargetFormat = ETextureRenderTargetFormat::RTF_Depth;
+	//DepthTexture->RenderTargetFormat = ETextureRenderTargetFormat
 	DepthTexture->bAutoGenerateMips = false;
 	DepthTexture->Filter = TF_Bilinear;
 	DepthTexture->bForceLinearGamma = true;
@@ -52,10 +66,10 @@ void AViewshedCamera::Tick(float DeltaTime)
 		return;
 	}
 
-	FMatrix ViewProjectionMatrix = ViewshedCamera->GetViewProjectionMatrix();
-	FMatrix GPUProjectionMatrix = AdjustProjectionMatrixForRHI(ViewProjectionMatrix);
-	UKismetRenderingLibrary::SetGlobalMatrixParameter(this, TEXT("_ArcGISViewshedViewProjectionMatrix"), GPUProjectionMatrix);
-	UKismetRenderingLibrary::SetGlobalFloatParameter(this, TEXT("_ArcGISViewshedFarPlane"), ViewshedCamera->FarPlane);
+	//FMatrix ViewProjectionMatrix = ViewshedCamera->GetViewProjectionMatrix();
+	//FMatrix GPUProjectionMatrix = AdjustProjectionMatrixForRHI(ViewProjectionMatrix);
+	//UKismetRenderingLibrary::SetGlobalMatrixParameter(this, TEXT("_ArcGISViewshedViewProjectionMatrix"), GPUProjectionMatrix);
+	//UKismetRenderingLibrary::SetGlobalFloatParameter(this, TEXT("_ArcGISViewshedFarPlane"), ViewshedCamera->FarPlane);
 
 	LastViewshedCameraPosition = GetActorLocation();
 	LastViewshedCameraRotation = GetActorRotation();
