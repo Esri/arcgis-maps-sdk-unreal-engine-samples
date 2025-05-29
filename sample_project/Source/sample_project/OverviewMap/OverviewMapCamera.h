9eb6dea523d75a870006bbd208f52e3c90d97a8c
Mason Gaw <89485705+mmgaw113@users.noreply.github.com>
2025-05-29 16:48:43 -0700

    New Sample: Overview map sample (#147)
    * Init
    
    * Icons
    
    * C++ and BP Samples
    
    * Add to sample switcher
    
    * Add read me and polish UI
    
    * Remove excess files
    
    * Update copy right
    
    * Adjust UI
    
    * Address feedback
    
    * Address feedback
    
    * Address feedback, fix toggle bug, fix copyright
    
    * Address feedback, fix toggle bug, fix copyright
    
    * Address Feedback
    
    * Address UI Feedback
    
    * UI Feedback

---
+++ sample_project/Source/sample_project/OverviewMap/OverviewMapCamera.h
@@ -0,0 +1,63 @@
+/* Copyright 2025 Esri
+*
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "GameFramework/Actor.h"
+#include "OverviewMapCamera.generated.h"
+
+class AArcGISPawn;
+class UArcGISLocationComponent;
+class UArcGISCameraComponent;
+class USceneCaptureComponent2D;
+
+UCLASS()
+class SAMPLE_PROJECT_API AOverviewMapCamera : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AOverviewMapCamera();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void UpdateLocation();
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta=(AllowPrivateAccess))
+	UArcGISCameraComponent* ArcGISCamera;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta=(AllowPrivateAccess))
+	AArcGISPawn* ArcGISPawn;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta=(AllowPrivateAccess))
+	UArcGISLocationComponent* pawnLocationComponent;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta=(AllowPrivateAccess))
+	UArcGISLocationComponent* LocationComponent;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta=(AllowPrivateAccess))
+	USceneCaptureComponent2D* SceneCaptureComponent;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess))
+	UTextureRenderTarget2D* Texture = LoadObject<UTextureRenderTarget2D>(nullptr,
+		TEXT("/Script/Engine.TextureRenderTarget2D'/Game/SampleViewer/Samples/OverviewMap/UI/OverviewMapRenderTarget.OverviewMapRenderTarget'"));
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess))
+	UUserWidget* UIWidget;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	TSubclassOf<class UUserWidget> UIWidgetClass;
+};
