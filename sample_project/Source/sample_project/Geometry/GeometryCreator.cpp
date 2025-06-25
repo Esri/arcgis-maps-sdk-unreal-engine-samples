d1a0f563e573ef0d66e7057e272cf5f88f74a89d
Jade Huang <jadehuang@esri.com>
2025-06-25 10:32:47 -0700

    Geometry: New sample (#151)
    * set up UI and code
    
    * finish polyline calculation
    
    * fix polygon rendering bug
    
    * make unit switching UI, change location
    
    * set up envelope
    
    * enable unit switching
    
    * lock pawn movement when pressing shift
    
    * add press release to input system
    
    * fix envelop bug, update envelope on tick
    
    * make UI stay clicked
    
    * fix envelope point render below terrain
    
    * add to sample viewer, fix notification UI
    
    * fix sample viewer envelop issue, code cleanup, fix SR
    
    * address comment, update UI images
    
    * unit switching on hover color changes
    
    * unit switching UI complete
    
    * finish UI feedback, fix crash, sort variables
    
    * fix triangle bug, change input manager, fix UI
    
    * fix color

---
+++ sample_project/Source/sample_project/Geometry/GeometryCreator.cpp
@@ -0,0 +1,461 @@
+// /* Copyright 2025 Esri* * Licensed under the Apache License Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+
+#include "GeometryCreator.h"
+
+#include "sample_project/InputManager.h"
+
+constexpr double Interval = 10000;
+constexpr int TraceLen = 1000000;
+constexpr int MarkerHeight = 300;
+
+AGeometryCreator::AGeometryCreator()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RouteMesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Game/SampleViewer/SharedResources/Geometries/Cube.Cube"));
+}
+
+void AGeometryCreator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MapActor = Cast<AArcGISMapActor>(UGameplayStatics::GetActorOfClass(GetWorld(), AArcGISMapActor::StaticClass()));
+
+	if (MapActor)
+	{
+		MapComponent = MapActor->GetMapComponent();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("ArcGISMapActor not found in the level!"));
+	}
+
+	SpatialReference = MapComponent->GetOriginPosition()->GetSpatialReference();
+
+	if (!InputManager)
+	{
+		InputManager = Cast<AInputManager>(UGameplayStatics::GetActorOfClass(GetWorld(), AInputManager::StaticClass()));
+	}
+
+	InputManager->OnInputTrigger.AddDynamic(this, &AGeometryCreator::StartGeometry);
+	InputManager->OnInputEnd.AddDynamic(this, &AGeometryCreator::EndGeometry);
+
+	auto playerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+
+	if (playerController)
+	{
+		playerController->bShowMouseCursor = true;
+		playerController->bEnableClickEvents = true;
+	}
+
+	if (UIWidgetClass)
+	{
+		UIWidget = CreateWidget<UUserWidget>(GetWorld(), UIWidgetClass);
+		if (!UIWidget)
+		{
+			return;
+		}
+
+		UIWidget->AddToViewport();
+		UnitDropdown = (UComboBoxString*)UIWidget->GetWidgetFromName(TEXT("UnitDropDown"));
+		if (UIWidget->FindFunction("ShowInstruction"))
+		{
+			UIWidget->ProcessEvent(UIWidget->FindFunction("ShowInstruction"), nullptr);
+		}
+	}
+
+	CurrentLinearUnit = UArcGISLinearUnit::CreateArcGISLinearUnit(EArcGISLinearUnitId::Miles);
+	CurrentAreaUnit = UArcGISAreaUnit::CreateArcGISAreaUnit(EArcGISAreaUnitId::SquareMiles);
+}
+
+void AGeometryCreator::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	InputManager->OnInputTrigger.RemoveDynamic(this, &AGeometryCreator::StartGeometry);
+	InputManager->OnInputEnd.RemoveDynamic(this, &AGeometryCreator::EndGeometry);
+}
+
+void AGeometryCreator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Continuously update visual cue in update
+	if (bIsEnvelopeMode && bIsDragging)
+	{
+		UpdateDraggingVisualization();
+	}
+}
+
+void AGeometryCreator::StartGeometry()
+{
+	FVector direction;
+	FHitResult hit;
+	FVector position;
+
+	auto PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	PlayerController->DeprojectMousePositionToWorld(position, direction);
+
+	bool bTraceSuccess =
+		GetWorld()->LineTraceSingleByChannel(hit, position, position + TraceLen * direction, ECC_Visibility, FCollisionQueryParams());
+
+	if (bTraceSuccess && hit.GetActor()->GetClass() == AArcGISMapActor::StaticClass() && hit.bBlockingHit)
+	{
+		SpawnParam.Owner = this;
+
+		auto lineMarker = GetWorld()->SpawnActor<ARouteMarker>(ARouteMarker::StaticClass(), hit.ImpactPoint, FRotator(0), SpawnParam);
+		auto hitPoint = MapComponent->TransformEnginePositionToPoint(hit.ImpactPoint);
+
+		if (bIsEnvelopeMode)
+		{
+			StartPoint = hitPoint;
+			bIsDragging = true;
+			Stops.Add(lineMarker);
+		}
+		else
+		{
+			if (bIsPolygonMode)
+			{
+				// Clear the last segment when adding a new polygon vertice
+				int32 RemovedCount = lastToStartInterpolationPoints.Num();
+				for (int32 i = 0; i <= RemovedCount; i++)
+				{
+					if (!SplineMeshComponents.IsEmpty())
+					{
+						auto Last = SplineMeshComponents.GetHead();
+						if (Last && IsValid(Last->GetValue()))
+						{
+							Last->GetValue()->UnregisterComponent();
+							Last->GetValue()->DestroyComponent();
+						}
+						SplineMeshComponents.RemoveNode(Last);
+					}
+				}
+
+				for (AActor* Point : lastToStartInterpolationPoints)
+				{
+					if (Point)
+						Point->Destroy();
+				}
+				lastToStartInterpolationPoints.Empty();
+			}
+
+			if (Stops.Num() > 0)
+			{
+				auto lastStop = Stops.Last();
+				auto lastPoint = MapComponent->TransformEnginePositionToPoint(lastStop->GetActorLocation());
+
+				if (bIsPolylineMode)
+				{
+					double distance =
+						UArcGISGeometryEngine::DistanceGeodetic(lastPoint, hitPoint, CurrentLinearUnit,
+																UArcGISAngularUnit::CreateArcGISAngularUnit(EArcGISAngularUnitId::Degrees),
+																EArcGISGeodeticCurveType::Geodesic)
+							->GetDistance();
+					Calculation += distance;
+					UpdateDisplay(Calculation);
+				}
+				//FeaturePoints array stores user-added points and interpolated points
+				FeaturePoints.Add(lastStop);
+				Interpolate(lastStop, lineMarker, FeaturePoints);
+				FeaturePoints.Add(lineMarker);
+			}
+			//Stops array stores user-added points
+			Stops.Add(lineMarker);
+
+			if (bIsPolygonMode)
+			{
+				//create polygon when vertice count is 3 or more
+				if (Stops.Num() >= 3)
+				{
+					//compute the last segment
+					Interpolate(lineMarker, Stops[0], lastToStartInterpolationPoints);
+					CreateAndCalculatePolygon();
+				}
+			}
+			//render line when vertice count is 2 or more
+			if (Stops.Num() >= 2)
+			{
+				RenderLine(FeaturePoints);
+			}
+		}
+	}
+}
+
+void AGeometryCreator::EndGeometry()
+{
+	if (bIsEnvelopeMode)
+	{
+		FVector direction;
+		FHitResult hit;
+		FVector position;
+
+		auto PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		PlayerController->DeprojectMousePositionToWorld(position, direction);
+
+		bool bTraceSuccess =
+			GetWorld()->LineTraceSingleByChannel(hit, position, position + TraceLen * direction, ECC_Visibility, FCollisionQueryParams());
+
+		if (bTraceSuccess && hit.GetActor()->GetClass() == AArcGISMapActor::StaticClass() && hit.bBlockingHit)
+		{
+			UArcGISPoint* EndPoint = MapComponent->TransformEnginePositionToPoint(hit.ImpactPoint);
+			CreateAndCalculateEnvelope(StartPoint, EndPoint);
+		}
+		bIsDragging = false;
+	}
+}
+
+void AGeometryCreator::RenderLine(TArray<AActor*>& Points)
+{
+	//allPoints contains FeaturePoints plus the last segment for polygons
+	TArray<AActor*> allPoints = Points;
+
+	if (bIsPolygonMode && Stops.Num() >= 3)
+	{
+		for (AActor* Point : lastToStartInterpolationPoints)
+		{
+			allPoints.Add(Point);
+		}
+
+		// Close polygon only after interpolated points are added
+		AActor* First = allPoints[0];
+		if (IsValid(First) && First->GetActorTransform().IsValid())
+		{
+			allPoints.Add(First);
+		}
+	}
+
+	for (int i = 1; i < allPoints.Num(); i++)
+	{
+		TObjectPtr<USplineMeshComponent> SplineMesh = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
+		SplineMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+		SplineMesh->RegisterComponent();
+		SplineMesh->SetMobility(EComponentMobility::Movable);
+
+		FVector start = allPoints[i - 1]->GetActorLocation();
+		FVector end = allPoints[i]->GetActorLocation();
+
+		FVector tangent = (end - start).GetSafeNormal() * 100;
+
+		SplineMesh->SetStartAndEnd(start, tangent, end, tangent);
+		SplineMesh->SetStartScale(RouteCueScale);
+		SplineMesh->SetEndScale(RouteCueScale);
+		SplineMesh->SetStaticMesh(RouteMesh);
+
+		SplineMeshComponents.AddHead(SplineMesh);
+	}
+}
+
+void AGeometryCreator::ClearLine()
+{
+	for (auto point : FeaturePoints)
+	{
+		if (point)
+			point->Destroy();
+	}
+
+	for (auto point : lastToStartInterpolationPoints)
+	{
+		if (point)
+			point->Destroy();
+	}
+
+	for (auto stop : Stops)
+	{
+		if(stop)
+			stop->Destroy();
+	}
+
+	FeaturePoints.Empty();
+	Stops.Empty();
+	Calculation = 0;
+
+	if (!SplineMeshComponents.IsEmpty())
+	{
+		for (auto point : SplineMeshComponents)
+		{
+			point->UnregisterComponent();
+			point->DestroyComponent();
+		}
+		SplineMeshComponents.Empty();
+	}
+}
+
+void AGeometryCreator::CreateAndCalculatePolygon()
+{
+	UArcGISPolygonBuilder* polygonBuilder = UArcGISPolygonBuilder::CreateArcGISPolygonBuilderFromSpatialReference(SpatialReference);
+
+	for (AActor* Stop : Stops)
+	{
+		UArcGISPoint* location = MapComponent->TransformEnginePositionToPoint(Stop->GetActorLocation());
+		polygonBuilder->AddPoint(location);
+	}
+
+	auto polygon = Cast<UArcGISGeometry>(polygonBuilder->ToGeometry());
+
+	Calculation = UArcGISGeometryEngine::AreaGeodetic(polygon, CurrentAreaUnit, EArcGISGeodeticCurveType::Geodesic);
+
+	UpdateDisplay(Calculation);
+}
+
+void AGeometryCreator::CreateAndCalculateEnvelope(UArcGISPoint* Start, UArcGISPoint* End)
+{
+	double MinX = FMath::Min(Start->GetX(), End->GetX());
+	double MinY = FMath::Min(Start->GetY(), End->GetY());
+	double MaxX = FMath::Max(Start->GetX(), End->GetX());
+	double MaxY = FMath::Max(Start->GetY(), End->GetY());
+
+	UArcGISEnvelopeBuilder* EnvelopeBuilder = UArcGISEnvelopeBuilder::CreateArcGISEnvelopeBuilderFromSpatialReference(SpatialReference);
+	EnvelopeBuilder->SetXY(MinX, MinY, MaxX, MaxY);
+
+	UArcGISGeometry* Envelope = EnvelopeBuilder->ToGeometry();
+
+	VisualizeEnvelope(MinX, MinY, MaxX, MaxY, SpatialReference);
+
+	Calculation = UArcGISGeometryEngine::AreaGeodetic(Envelope, CurrentAreaUnit, EArcGISGeodeticCurveType::Geodesic);
+
+	UpdateDisplay(Calculation);
+}
+
+void AGeometryCreator::VisualizeEnvelope(double MinX, double MinY, double MaxX, double MaxY, UArcGISSpatialReference* SpatialRef)
+{
+	ClearLine();
+
+	TArray<UArcGISPoint*> Corners;
+	Corners.Add(UArcGISPoint::CreateArcGISPointWithXYSpatialReference(MinX, MinY, SpatialRef)); // Bottom Left
+	Corners.Add(UArcGISPoint::CreateArcGISPointWithXYSpatialReference(MaxX, MinY, SpatialRef)); // Bottom Right
+	Corners.Add(UArcGISPoint::CreateArcGISPointWithXYSpatialReference(MaxX, MaxY, SpatialRef)); // Top Right
+	Corners.Add(UArcGISPoint::CreateArcGISPointWithXYSpatialReference(MinX, MaxY, SpatialRef)); // Top Left
+
+	TArray<AActor*> markers;
+
+	for (UArcGISPoint* Corner : Corners)
+	{
+		FVector FlatWorldPos = MapComponent->TransformPointToEnginePosition(Corner);
+		// Raycast to get terrain surface elevation
+		FVector RayStart = FlatWorldPos + FVector(0, 0, 5000);
+		FVector RayEnd = FlatWorldPos - FVector(0, 0, 5000);
+
+		FHitResult Hit;
+		if (GetWorld()->LineTraceSingleByChannel(Hit, RayStart, RayEnd, ECC_Visibility))
+		{
+			FVector AdjustedWorldPos = Hit.ImpactPoint + FVector(0, 0, MarkerHeight);
+
+			SpawnParam.Owner = this;
+			auto marker = GetWorld()->SpawnActor<ARouteMarker>(ARouteMarker::StaticClass(), AdjustedWorldPos, FRotator(0), SpawnParam);
+
+			if (marker)
+			{
+				markers.Add(marker);
+				Stops.Add(marker);
+			}
+		}
+	}
+
+	if (markers.Num() < 4)
+	{
+		return;
+	}
+
+	// Connect corners and interpolate
+	for (int32 i = 0; i < markers.Num(); ++i)
+	{
+		AActor* currentMarker = markers[i];
+		AActor* nextMarker = markers[(i + 1) % markers.Num()];
+
+		FeaturePoints.Add(currentMarker);
+		Interpolate(currentMarker, nextMarker, FeaturePoints);
+	}
+
+	FeaturePoints.Add(markers[0]);
+
+	RenderLine(FeaturePoints);
+}
+
+//update the visualization while dragging
+void AGeometryCreator::UpdateDraggingVisualization()
+{
+	FVector WorldOrigin;
+	FVector WorldDirection;
+	FVector MousePosition;
+
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (!PlayerController || !PlayerController->DeprojectMousePositionToWorld(WorldOrigin, WorldDirection))
+	{
+		return;
+	}
+
+	FHitResult Hit;
+	FVector TraceEnd = WorldOrigin + (TraceLen * WorldDirection);
+
+	if (GetWorld()->LineTraceSingleByChannel(Hit, WorldOrigin, TraceEnd, ECC_Visibility))
+	{
+		if (Hit.bBlockingHit && Hit.GetActor()->IsA<AArcGISMapActor>())
+		{
+			UArcGISPoint* CurrentPoint = MapComponent->TransformEnginePositionToPoint(Hit.ImpactPoint);
+
+			CreateAndCalculateEnvelope(StartPoint, CurrentPoint);
+		}
+	}
+}
+
+float AGeometryCreator::GetCalculation()
+{
+	return Calculation;
+}
+
+void AGeometryCreator::SetCalculation(float calculation)
+{
+	Calculation = calculation;
+}
+
+UArcGISLinearUnit* AGeometryCreator::GetLinearUnit()
+{
+	return CurrentLinearUnit;
+}
+
+UArcGISAreaUnit* AGeometryCreator::GetAreaUnit()
+{
+	return CurrentAreaUnit;
+}
+
+void AGeometryCreator::SetLinearUnit(UArcGISLinearUnit* LU)
+{
+	CurrentLinearUnit = LU;
+}
+
+void AGeometryCreator::SetAreaUnit(UArcGISAreaUnit* AU)
+{
+	CurrentAreaUnit = AU;
+}
+
+void AGeometryCreator::UpdateDisplay(float Value)
+{
+	if (UIWidget)
+	{
+		UTextBlock* ResultText = Cast<UTextBlock>(UIWidget->GetWidgetFromName(TEXT("Result")));
+		if (ResultText)
+		{
+			FString FormattedString = FString::Printf(TEXT("%.2f"), Value);
+			ResultText->SetText(FText::FromString(FormattedString));
+		}
+	}
+}
+
+//Interpolate points between two given points, then returns an array of points excluding the two
+void AGeometryCreator::Interpolate(AActor* start, AActor* end, TArray<AActor*>& Points)
+{
+	FVector A = start->GetActorLocation();
+	FVector B = end->GetActorLocation();
+	float Dist = FVector::Dist(A, B);
+	int32 Count = FMath::FloorToInt(Dist / InterpolationInterval);
+
+	for (int32 i = 1; i < Count; ++i)
+	{
+		SpawnParam.Owner = this;
+		FVector nextInterpolationPos = FMath::Lerp(A, B, i / static_cast<float>(Count));
+		AActor* nextInterpolation =
+			GetWorld()->SpawnActor<ABreadcrumb>(ABreadcrumb::StaticClass(), nextInterpolationPos, FRotator::ZeroRotator, SpawnParam);
+		Points.Add(nextInterpolation);
+	}
+}
---
+++ sample_project/Source/sample_project/Geometry/GeometryCreator.h
@@ -0,0 +1,140 @@
+// /* Copyright 2025 Esri* * Licensed under the Apache License Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+
+#pragma once
+
+#include "ArcGISMapsSDK/Actors/ArcGISMapActor.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISAngularUnit.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISAngularUnitId.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISAreaUnit.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISEnvelopeBuilder.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISGeodeticDistanceResult.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISGeometryEngine.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISLinearUnit.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISPoint.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISPolygonBuilder.h"
+#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISSpatialReference.h"
+#include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "ArcGISMapsSDK/Components/ArcGISMapComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/ComboBoxString.h"
+#include "Components/SplineMeshComponent.h"
+#include "Components/TextBlock.h"
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "sample_project/Routing/Breadcrumb.h"
+#include "sample_project/Routing/RouteMarker.h"
+
+#include "GeometryCreator.generated.h"
+
+class UArcGISCameraControllerComponent;
+class UArcGISLocationComponent;
+class UArcGISMapComponent;
+class ULineBatchComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class AGeometryCreator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGeometryCreator();
+
+	UPROPERTY(BlueprintReadWrite)
+	float Calculation = 0.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UUserWidget> UIWidget;
+	UPROPERTY()
+	TObjectPtr<UComboBoxString> UnitDropdown;
+
+	UFUNCTION(BlueprintCallable)
+	void SetCalculation(float InCalculation);
+
+	UFUNCTION(BlueprintCallable)
+	float GetCalculation();
+
+	UFUNCTION(BlueprintCallable)
+	void SetLinearUnit(UArcGISLinearUnit* LU);
+
+	UFUNCTION(BlueprintCallable)
+	void SetAreaUnit(UArcGISAreaUnit* AU);
+
+	UFUNCTION(BlueprintCallable)
+	UArcGISLinearUnit* GetLinearUnit();
+
+	UFUNCTION(BlueprintCallable)
+	UArcGISAreaUnit* GetAreaUnit();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	bool bIsDragging = false;
+	float InterpolationInterval = 10000;
+	FActorSpawnParameters SpawnParam = FActorSpawnParameters();
+	FString UnitText;
+	FVector2D RouteCueScale = FVector2D(5);
+	UStaticMesh* RouteMesh;
+	TDoubleLinkedList<USplineMeshComponent*> SplineMeshComponents;
+
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess))
+	bool bIsEnvelopeMode = false;
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess))
+	bool bIsPolygonMode = false;
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess))
+	bool bIsPolylineMode = true;
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess))
+	TObjectPtr<UArcGISAreaUnit> CurrentAreaUnit;
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess))
+	TObjectPtr<UArcGISLinearUnit> CurrentLinearUnit;
+	UPROPERTY()
+	TArray<AActor*> FeaturePoints;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess))
+	class AInputManager* InputManager;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> InterpolationMarkerPrefab;
+	UPROPERTY()
+	FVector LastRootPosition;
+	UPROPERTY()
+	TArray<AActor*> lastToStartInterpolationPoints;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> LineMarkerPrefab;
+	UPROPERTY(meta = (AllowPrivateAccess))
+	AArcGISMapActor* MapActor;
+	UPROPERTY(meta = (AllowPrivateAccess))
+	TObjectPtr<UArcGISMapComponent> MapComponent;
+	UPROPERTY()
+	UArcGISSpatialReference* SpatialReference;
+	UPROPERTY()
+	TObjectPtr<UArcGISPoint> StartPoint;
+	UPROPERTY()
+	TArray<ARouteMarker*> Stops;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess))
+	TSubclassOf<UUserWidget> UIWidgetClass;
+
+	void CreateAndCalculatePolygon();
+	void CreateAndCalculateEnvelope(UArcGISPoint* Start, UArcGISPoint* End);
+	void Interpolate(AActor* Start, AActor* End, TArray<AActor*>& Points);
+	void VisualizeEnvelope(double MinX, double MinY, double MaxX, double MaxY, UArcGISSpatialReference* SpatialRef);
+	void UpdateDraggingVisualization();
+
+	UFUNCTION()
+	void StartGeometry();
+
+	UFUNCTION()
+	void EndGeometry();
+
+	UFUNCTION(BlueprintCallable)
+	void RenderLine(TArray<AActor*>& Points);
+
+	UFUNCTION(BlueprintCallable)
+	void ClearLine();
+
+	UFUNCTION()
+	void UpdateDisplay(float Value);
+};
