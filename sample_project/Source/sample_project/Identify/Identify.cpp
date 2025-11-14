c2fd976b6db5ff712540534e7a34a01346ed7359
Jade Huang <jadehuang@esri.com>
2025-11-14 14:32:13 -0800

    update API

--- sample_project/Source/sample_project/Identify/Identify.cpp
+++ sample_project/Source/sample_project/Identify/Identify.cpp
@@ -1,43 +1,24 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
+﻿// /* Copyright 2025 Esri* * Licensed under the Apache License Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
 
 #include "Identify.h"
-#include "ArcGISMapsSDK/Components/ArcGISMapComponent.h"
 #include "Engine/World.h"
 #include "Kismet/GameplayStatics.h"
-#include "sample_project/InputManager.h"
 
-#include "ArcGISMapsSDK/API/ArcGISRuntime/Data/ArcGISFeature.h"
-#include "ArcGISMapsSDK/API/GameEngine/Elevation/Base/ArcGISElevationSource.h"
-#include "ArcGISMapsSDK/API/GameEngine/Geometry/ArcGISPoint.h"
-#include "ArcGISMapsSDK/API/GameEngine/Layers/Base/ArcGISLayer.h"
+#include "ArcGISMapsSDK/API/GameEngine/Attributes/ArcGISAttributeValue.h"
 #include "ArcGISMapsSDK/API/GameEngine/Map/ArcGISGeoElement.h"
 #include "ArcGISMapsSDK/API/GameEngine/MapView/ArcGISIdentifyLayerResult.h"
-#include "ArcGISMapsSDK/API/GameEngine/View/ArcGISGeoElementImmutableCollection.h"
-#include "ArcGISMapsSDK/API/GameEngine/View/ArcGISIdentifyLayerResultImmutableCollection.h"
 #include "ArcGISMapsSDK/API/GameEngine/View/ArcGISView.h"
-#include "ArcGISMapsSDK/API/GameEngine/View/State/ArcGISElevationSourceViewState.h"
-#include "ArcGISMapsSDK/API/GameEngine/View/State/ArcGISLayerViewState.h"
-#include "ArcGISMapsSDK/API/GameEngine/View/State/ArcGISViewState.h"
-#include "ArcGISMapsSDK/API/GameEngine/View/State/ArcGISViewStateMessage.h"
 #include "ArcGISMapsSDK/API/Standard/ArcGISElement.h"
+#include "ArcGISMapsSDK/API/Unreal/ArcGISException.h"
 #include "ArcGISMapsSDK/API/Unreal/ArcGISFuture.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Elevation/ArcGISImageElevationSource.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Extent/ArcGISExtentCircle.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Geometry/ArcGISSpatialReference.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Layers/ArcGIS3DObjectSceneLayer.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Layers/ArcGISImageLayer.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Layers/Base/ArcGISLayerCollection.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Map/ArcGISBasemap.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Map/ArcGISMap.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Map/ArcGISMapElevation.h"
-#include "ArcGISMapsSDK/BlueprintNodes/GameEngine/Map/ArcGISMapType.h"
+#include "ArcGISMapsSDK/API/Unreal/ArcGISImmutableCollection.h"
 #include "ArcGISMapsSDK/CAPI/Invoke.h"
 #include "ArcGISMapsSDK/Components/ArcGISLocationComponent.h"
+#include "ArcGISMapsSDK/Components/ArcGISMapComponent.h"
+#include "ArcGISMapsSDK/Utils/ArcGISViewCoordinateTransformer.h"
+#include "sample_project/InputManager.h"
 
-#include <ArcGISMapsSDK/Utils/ArcGISViewCoordinateTransformer.h>
 #include "ArcGISPawn.h"
-
 // Sets default values
 AIdentify::AIdentify()
 {
@@ -89,13 +70,69 @@ void AIdentify::Tick(float DeltaTime)
 
 }
 
+FString GetStringFromAttributeValue(const Esri::GameEngine::Attributes::ArcGISAttributeValue& attributeValue)
+{
+	auto attributeType = attributeValue.GetAttributeValueType();
+
+	if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::DateTime)
+	{
+		return attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::DateTime>().ToString();
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Float32)
+	{
+		return FString::SanitizeFloat(attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Float32>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Float64)
+	{
+		return FString::SanitizeFloat(attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Float64>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::GUID)
+	{
+		return attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::GUID>().ToString();
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Int16)
+	{
+		return FString::Printf(TEXT("%d"), attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Int16>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Int32)
+	{
+		return FString::Printf(TEXT("%d"), attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Int32>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Int64)
+	{
+		return FString::Printf(TEXT("%lld"), attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Int64>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Float64)
+	{
+		return FString::SanitizeFloat(attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Float64>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::String)
+	{
+		return attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::String>();
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Uint16)
+	{
+		return FString::Printf(TEXT("%d"), attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Uint16>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Uint32)
+	{
+		return FString::Printf(TEXT("%d"), attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Uint32>());
+	}
+	else if (attributeType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::Uint64)
+	{
+		return FString::Printf(TEXT("%lld"), attributeValue.GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::Uint64>());
+	}
+
+	return "<unknown-type>";
+}
+
+
 FString AIdentify::IdentifyAtMouseClick()
 {
-	FString outputString = "No output";
 	APlayerController* PlayerController = Cast<APlayerController>(GetWorld()->GetFirstPlayerController());
 	if (!PlayerController)
 	{
-		return outputString;
+		return "Null player controller";
 	}
 
 	FVector Location, Direction;
@@ -106,7 +143,7 @@ FString AIdentify::IdentifyAtMouseClick()
 	if (!UKismetSystemLibrary::LineTraceSingle(GetWorld(), Location, Location + Direction * 10000000.0, TraceTypeQuery1, false, ActorsToIgnore,
 											   EDrawDebugTrace::None, HitResult, true))
 	{
-		return outputString;
+		return "Line trace did not hit any actor";
 	}
 
 	auto geoPosition = MapComponent->TransformEnginePositionToPoint(HitResult.TraceStart)->APIObject;
@@ -115,50 +152,82 @@ FString AIdentify::IdentifyAtMouseClick()
 	auto point1 = Esri::GameEngine::Geometry::ArcGISPoint(std::move(geoPosition->GetHandle()));
 	auto point2 = Esri::GameEngine::Geometry::ArcGISPoint(std::move(geoPosition2->GetHandle()));
 
-	TSharedPtr<Esri::GameEngine::View::ArcGISView> view = MapComponent->GetView()->APIObject;
+	auto view = MapComponent->GetView()->APIObject;
+
 	try
 	{
 		auto results = view->IdentifyLayersAsync(point1, point2, -1);
 		point1.SetHandle(nullptr);
 		point2.SetHandle(nullptr);
 
-		auto futureResults = results.Get();
-		if (!futureResults)
-		{
-			return outputString;
-		}
-		if (futureResults.GetSize() == 0)
-		{
-			return outputString;
-		}
-		auto singleResult = futureResults.At(0);
+		auto identifyLayerResults = results.Get();
 
-		auto geoElements = singleResult.GetGeoElementsImmutableCollection();
-		if (geoElements.GetSize() == 0)
+		if (!identifyLayerResults)
 		{
-			return outputString;
-		}
-		auto feature = geoElements.At(0);
-
-		auto tmap = feature->GetAttributes();
-		auto nameAttributeValue = tmap.Find("NAME");
-		auto nameType = nameAttributeValue->GetAttributeValueType();
-
-		if (nameType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::String)
-		{
-			auto name = nameAttributeValue->GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::String>();
-			outputString = "Name: " + name;
+			return "Null identify layer results collection";
 		}
 
-		auto dateAttributeValue = tmap.Find("LSTMODDATE");
-		auto dateType = dateAttributeValue->GetAttributeValueType();
-		
-		if (dateType == Esri::GameEngine::Attributes::ArcGISAttributeValueType::DateTime)
+		if (identifyLayerResults.GetSize() == 0)
 		{
-			auto date = dateAttributeValue->GetValue<Esri::GameEngine::Attributes::ArcGISAttributeValueType::DateTime>();
-			auto dateString = date.ToString();
-			outputString = "Date: " + dateString;
+			return "Empty identify layer results collection";
 		}
+
+		auto identifyLayerResultsSize = identifyLayerResults.GetSize();
+
+		FString outputString = "[";
+
+		for (int i = 0; i < identifyLayerResultsSize; i++)
+		{
+			outputString += "[";
+
+			auto identifyLayerResult = identifyLayerResults.At(i);
+
+			auto geoElements = identifyLayerResult.GetGeoElements();
+
+			auto geoElementsSize = geoElements.GetSize();
+
+			for (int j = 0; j < geoElementsSize; j++)
+			{
+				outputString += "{";
+
+				auto feature = geoElements.At(j);
+
+				auto attributes = feature.GetAttributes();
+
+				auto attributeKeys = attributes.GetKeys();
+
+				for (int k = 0; k < attributeKeys.Num(); k++)
+				{
+					auto attributeKey = attributeKeys[k];
+					auto attributeValue = attributes.At(attributeKey);
+
+					outputString += "\"" + attributeKey + "\": \"" + GetStringFromAttributeValue(attributeValue) + "\"";
+
+					if (k < attributeKeys.Num() - 1)
+					{
+						outputString += ", ";
+					}
+				}
+
+				outputString += "}";
+
+				if (j < geoElementsSize - 1)
+				{
+					outputString += ", ";
+				}
+			}
+
+			outputString += "]";
+
+			if (i < identifyLayerResultsSize - 1)
+			{
+				outputString += ", ";
+			}
+		}
+
+		outputString += "]";
+
+		return outputString;
 	}
 	catch (Esri::Unreal::ArcGISException exception)
 	{
@@ -166,7 +235,6 @@ FString AIdentify::IdentifyAtMouseClick()
 		point2.SetHandle(nullptr);
 		return "Error: " + exception.GetMessage();
 	}
-	return outputString;
 }
 
 void AIdentify::OnInputTriggered()
