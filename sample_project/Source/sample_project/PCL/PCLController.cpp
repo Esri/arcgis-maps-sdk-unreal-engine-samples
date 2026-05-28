fd1ffd861136a03b8eb1aedb44c0aecf7b98f51f
Jade Huang <jadehuang@esri.com>
2026-05-27 18:09:17 -0700

    set up initial folders

---
+++ sample_project/Source/sample_project/PCL/PCLController.cpp
@@ -0,0 +1,27 @@
+// /* Copyright 2023 Esri* * Licensed under the Apache License Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+
+
+#include "PCLController.h"
+
+// Sets default values
+APCLController::APCLController()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APCLController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APCLController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
